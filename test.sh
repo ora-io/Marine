30b9e2c647ff28a3af8815752c6f56aea8208e4a
Riu <sleepygirl2023@gmail.com>
2023-10-09 15:38:44 +0800

    feature: update README

--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@
 # Then run `sh test.sh`
 
 npm run compile-local &&
-npm run exec-local -- 2279547 &&
+npm run exec-local -- 2279547
+
+npm run marine -- 0x52ea46506b9cc5ef470c5bf89f17dc28bb35d85c
+
+
 # npm run prove-local -- --inputgen 2279547 a60ecf32309539dd84f27a9563754dca818b815e &&
 # npm run prove-local -- --test 2279547 a60ecf32309539dd84f27a9563754dca818b815e
